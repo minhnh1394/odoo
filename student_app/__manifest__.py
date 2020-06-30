ad1e0b4152bee96b7e1758fa17c0ba57d3ebb5ca
minhnh1394 <67032794+minhnh1394@users.noreply.github.com>
2020-06-30 15:25:27 +0700

    ok

---
+++ student_app/__manifest__.py
@@ -0,0 +1,12 @@
+{    
+    'name': 'Student Management',    
+    'description': 'Manage student\'s general infomation.',    
+    'author': 'NHM',    
+    'depends': ['base'],  
+    'license':'AGPL-3',
+    'data': [
+ 
+    ],  
+    'application': True, 
+    'installable':True,
+} 
